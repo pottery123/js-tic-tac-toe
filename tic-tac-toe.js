16d5880efc361eebaabbcc1c8e30b44f8dbfd16c
Sarah <sarahkane682@yahoo.com>
2016-06-30 16:42:25 -0700

    working on win function

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,58 +1,162 @@
 function TicTacToe(callback) {
   this.turn = 0
-  this.callback = callback
+  this.player_1_score = []
+  this.player_2_score = []
 
-  this.turn = function(){
+  this.scores = [1,2,3,4,5,6,7,8,9]
+  this.winnerArray = {
+    a:[1,2,3],
+    b:[4,5,6],
+    c:[7,8,9],
+    d:[1,4,7],
+    e:[2,5,8],
+    f:[3,6,9],
+    g:[3,5,7],
+    h:[1,5,9]
+
+    }
+
+
+
+}
+
+TicTacToe.prototype.play = function(score){// all thing that happen when player plays
     this.turn += 1
+  if(this.turn %2 == 0){
+    var player2  = this.player_2_score.push(score)
+    return player2
+  }else{
 
+    var player1 =  this.player_1_score.push(score)
+    return player1
   }
 }
 
-TicTacToe.prototype.turn = function(){
+TicTacToe.prototype.currentPlayer = function(){
+  if(this.turn %2 == 0){
+    return this.player_2_score
+  }else{
+    return this.player_1_score
+  }
 }
 
-exports.TicTacToe
+ // TicTacToe.prototype.won = function(arrayScore){
+ //   for (variable in object)
+ // }
+
+
+
+
+
+
+
+
+
+TicTacToe.prototype.score1 = function(){
+  return this.scores[0]
+}
+
+TicTacToe.prototype.score2 = function(){
+  return this.scores[1]
+}
+
+TicTacToe.prototype.score3 = function(){
+  return this.scores[2]
+}
+
+TicTacToe.prototype.score4 = function(){
+  return this.scores[3]
+}
+
+TicTacToe.prototype.score5 = function(){
+  return this.scores[4]
+}
+
+TicTacToe.prototype.score6 = function(){
+  return this.scores[5]
+}
+
+TicTacToe.prototype.score7 = function(){
+  return this.scores[6]
+}
+
+TicTacToe.prototype.score8 = function(){
+  return this.scores[7]
+}
+
+TicTacToe.prototype.score9 = function(){
+  return this.scores[8]
+}
+
+
+
+
+
+
+
+
 
 
 
 $(document).ready( function() {
-var TicTacToe = new TicTacToe()
 
+var ticTactoe = new TicTacToe()
 
-  $("button.square_1").on('click', function(event){
-    console.log(this.turn)
+  $("button.square_1").one('click', function(event){
+    $(this).addClass('red');
+    ticTactoe.play(ticTactoe.score1())
+    console.log(ticTactoe.currentPlayer())
   })
 
-  $("button.square_2").on('click', function(event){
-    console.log(event, "yes2")
+  $("button.square_2").one('click', function(event){
+      $(this).addClass('red');
+
+      ticTactoe.play(ticTactoe.score2())
+      console.log(ticTactoe.currentPlayer())
+
   })
 
-  $("button.square_3").on('click', function(event){
-    console.log(event, "yes3")
+  $("button.square_3").one('click', function(event){
+    $(this).addClass('red');
+    ticTactoe.play(ticTactoe.score3())
+    console.log(ticTactoe.currentPlayer())
   })
 
-  $("button.square_4").on('click', function(event){
-    console.log(event, "yes4")
+  $("button.square_4").one('click', function(event){
+    $(this).addClass('red');
+    ticTactoe.play(ticTactoe.score4())
+    console.log(ticTactoe.currentPlayer())
   })
 
-  $("button.square_5").on('click', function(event){
-    console.log(event, "yes5")
+  $("button.square_5").one('click', function(event){
+    $(this).addClass('red');
+    ticTactoe.play(ticTactoe.score5())
+    console.log(ticTactoe.currentPlayer())
   })
 
-  $("button.square_6").on('click', function(event){
-    console.log(event, "yes6")
+  $("button.square_6").one('click', function(event){
+    $(this).addClass('red');
+    ticTactoe.play(ticTactoe.score6())
+    console.log(ticTactoe.currentPlayer())
   })
 
-  $("button.square_7").on('click', function(event){
-    console.log(event, "yes7")
+  $("button.square_7").one('click', function(event){
+    $(this).addClass('red');
+    ticTactoe.play(ticTactoe.score7())
+    console.log(ticTactoe.currentPlayer())
   })
 
-  $("button.square_8").on('click', function(event){
-    console.log(event, "yes8")
+  $("button.square_8").one('click', function(event){
+    $(this).addClass('red');
+    ticTactoe.play(ticTactoe.score8())
+    console.log(ticTactoe.currentPlayer())
   })
 
-  $("button.square_9").on('click', function(event){
-    console.log(event, "yes9")
+  $("button.square_9").one('click', function(event){
+    $(this).addClass('red');
+    ticTactoe.play(ticTactoe.score9())
+    console.log(ticTactoe.currentPlayer())
+
   })
 
 
--- turns.md
+++ turns.md
@@ -15,9 +15,32 @@
 
 
 
-
 1: get board working
     -HTML
     -CSS
     -JavaScritp
     -JQ
+
+
+
+  2: every time a player presses the button the turn  increments by one
+      if counter is a even number that is one turn
+      if counter is a odd number that is a different turn
+
+      var player_1_score if counter is even, player 1 score starts at zero
+      var player_2_score if counter is odd, player 2 score starts at zero
+
+
+  3: chart key value pairs correspond to buttons
+      var chart = {
+        button1 : 1
+        button2 : 2
+        button3 : 3
+        button4 : 4
+      }
+
+  4: when player clicks the number they picked is pushed into an array
+      when counter == 9 turns are over
+      the player who wins has the combination of numbers in the right order
+        ex. player one has won their array looks like this [1,2,3]
+        player two has not one their array looks like this [4,7,8]
