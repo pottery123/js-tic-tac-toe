d876bab379e0200ea192a4ceb357d1c6dd4a8359
Sarah <sarahkane682@yahoo.com>
2016-06-30 13:06:17 -0700

    working on turns

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,59 @@
-function TicTacToe() {
+function TicTacToe(callback) {
+  this.turn = 0
+  this.callback = callback
 
+  this.turn = function(){
+    this.turn += 1
+
+  }
 }
 
-TicTacToe.prototype = {
-  
+TicTacToe.prototype.turn = function(){
 }
+
+exports.TicTacToe
+
+
+
+$(document).ready( function() {
+var TicTacToe = new TicTacToe()
+
+
+  $("button.square_1").on('click', function(event){
+    console.log(this.turn)
+  })
+
+  $("button.square_2").on('click', function(event){
+    console.log(event, "yes2")
+  })
+
+  $("button.square_3").on('click', function(event){
+    console.log(event, "yes3")
+  })
+
+  $("button.square_4").on('click', function(event){
+    console.log(event, "yes4")
+  })
+
+  $("button.square_5").on('click', function(event){
+    console.log(event, "yes5")
+  })
+
+  $("button.square_6").on('click', function(event){
+    console.log(event, "yes6")
+  })
+
+  $("button.square_7").on('click', function(event){
+    console.log(event, "yes7")
+  })
+
+  $("button.square_8").on('click', function(event){
+    console.log(event, "yes8")
+  })
+
+  $("button.square_9").on('click', function(event){
+    console.log(event, "yes9")
+  })
+
+
+})
---
+++ turns.md
@@ -0,0 +1,23 @@
+#what constitutes a turn?
+
+
+-determine player turn
+-select a space
+-space available
+-space advantageous
+-claim space using player identifier
+-check win/loss/draw state
+  -what is a win
+  -what is a draw
+  -what is a loss
+  -communicate outcome
+  -continue play
+
+
+
+
+1: get board working
+    -HTML
+    -CSS
+    -JavaScritp
+    -JQ
